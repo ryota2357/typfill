b7f18075c6142dd5cdb9fef321b8593a964a4e94
ryota2357 <noreply@ryota2357.com>
2026-04-17 08:46:54 +0900

    setup

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nodejs-slim_24
+            pnpm
+
+            typescript-language-server
+            svelte-language-server
+            # vscode-css-languageserver
+            vscode-json-languageserver
+            nil
+          ];
+        };
+      }
+    );
+}
